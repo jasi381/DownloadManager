41584a56915b887c6da6cc0069ff77d9bd3aaa7a
jasi05072001 <139204957+jasi05072001@users.noreply.github.com>
2023-08-09 13:54:57 +0530

    multiple video downloading support added

--- app/src/main/java/com/jasmeet/myapplication/MainActivity.kt
+++ app/src/main/java/com/jasmeet/myapplication/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.jasmeet.myapplication
 
 import android.app.DownloadManager
-import android.content.Context
 import android.os.Bundle
 import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -30,6 +33,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jasmeet.myapplication.downloadManager.DownloadManagerClass
 import com.jasmeet.myapplication.ui.theme.MyApplicationTheme
@@ -49,7 +54,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    DownloadSection3(this)
+                    DownloadScreen()
 
                 }
             }
@@ -58,8 +63,130 @@ class MainActivity : ComponentActivity() {
 
 }
 
+//@Composable
+//fun DownloadSection33(context: Context) {
+//    val downloadProgress = remember { mutableStateOf(0.0f) }
+//    val downloadedBytes = remember { mutableStateOf(0L) }
+//    val fileSizeMB = remember { mutableStateOf(0L) }
+//
+//    val showProgressBar = remember { mutableStateOf(false) }
+//
+//    val animatedProgress by animateFloatAsState(
+//        targetValue = downloadProgress.value,
+//        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec, label = ""
+//    )
+//
+//    val downloadManagerClass = DownloadManagerClass(context)
+//    val scope = rememberCoroutineScope()
+//
+//    val downloadId2 = remember { mutableStateOf(0L) }
+//    val isFileDownloaded = remember { mutableStateOf(false) }
+//
+//    Column(
+//        modifier = Modifier.fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        LinearProgressIndicator(
+//            progress = animatedProgress,
+//            color = Color.Red,
+//            trackColor = Color.Yellow
+//        )
+//
+//        Spacer(modifier = Modifier.padding(top = 18.dp))
+//
+//        Row(
+//            horizontalArrangement = Arrangement.SpaceEvenly,
+//            modifier = Modifier.fillMaxWidth()
+//        ) {
+//            Button(
+//                onClick = {
+//                    scope.launch {
+//                        val fileName = "videos.mp4"
+//                        downloadId2.value = downloadManagerClass.startDownload(
+//                            "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4",
+//                            fileName
+//                        )
+//                        showProgressBar.value = true
+//                    }
+//                }
+//            ) {
+//                Text(text = "Download")
+//            }
+//            Button(
+//                onClick = {
+//                    scope.launch {
+//                        downloadManagerClass.cancelDownload(downloadId2.value)
+//                        showProgressBar.value = false
+//                        downloadProgress.value = 0.0f
+//                        downloadId2.value = 0L
+//                        downloadedBytes.value = 0L
+//                        fileSizeMB.value = 0L
+//
+//                        // Delete the downloaded file from storage
+//                        val file = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
+//                        file?.listFiles()?.forEach {
+//                            if (it.name == "video.mp4") {
+//                                it.delete()
+//                            }
+//                        }
+//                    }
+//                },
+//                enabled = !isFileDownloaded.value
+//            ) {
+//                Text(text = "Cancel")
+//            }
+//        }
+//
+//        if (downloadId2.value == 1L) {
+//            Text(text = "Download Completed")
+//            isFileDownloaded.value = true
+//        }
+//        if (showProgressBar.value) {
+//            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+//        }
+//        if (downloadedBytes.value > 0 && fileSizeMB.value > 0) {
+//            val downloadedMB = downloadedBytes.value / (1024 * 1024)
+//            val text = "${downloadedMB}MB/${fileSizeMB.value}MB"
+//            Text(text = text)
+//        }
+//    }
+//
+//    LaunchedEffect(downloadId2.value) {
+//        while (downloadId2.value != 0L) {
+//            when (downloadManagerClass.getDownloadStatus(downloadId2.value)) {
+//                DownloadManager.STATUS_SUCCESSFUL -> {
+//                    showProgressBar.value = false
+//                    downloadedBytes.value = downloadManagerClass.getDownloadedBytes(downloadId2.value)
+//                    downloadProgress.value = 1.0f
+//                    Utils.showToast(context, "Download Completed")
+//                }
+//                DownloadManager.STATUS_FAILED -> {
+//                    downloadProgress.value = 0.0f
+//                    showProgressBar.value = false
+//                    Utils.showToast(context, "Download Failed")
+//                }
+//                else -> {
+//                    val progress = downloadManagerClass.getDownloadProgress(downloadId2.value)
+//                    if (progress > 0) {
+//                        showProgressBar.value = true
+//                        downloadProgress.value = progress / 100f
+//                        fileSizeMB.value = downloadManagerClass.getFileSizeMB(downloadId2.value)
+//                        downloadedBytes.value = downloadManagerClass.getDownloadedBytes(downloadId2.value)
+//                    }
+//                }
+//            }
+//            delay(50)
+//        }
+//    }
+//
+//
+//}
+
+
 @Composable
-fun DownloadSection3(context: Context) {
+fun DownloadSection3() {
+    val context = LocalContext.current
     val downloadProgress = remember { mutableStateOf(0.0f) }
     val downloadedBytes = remember { mutableStateOf(0L) }
     val fileSizeMB = remember { mutableStateOf(0L) }
@@ -68,14 +195,27 @@ fun DownloadSection3(context: Context) {
 
     val animatedProgress by animateFloatAsState(
         targetValue = downloadProgress.value,
-        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec, label = ""
+        animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy), label = ""
     )
 
-    val downloadManagerClass = DownloadManagerClass(context)
+    val singleDownloadUrlList = listOf(
+        "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4",
+        "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4",
+        "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4"
+    )
+    val fileNames = listOf(
+        "video1.mp4",
+        "video2.mp4",
+        "video3.mp4"
+    )
+
     val scope = rememberCoroutineScope()
 
-    val downloadId2 = remember { mutableStateOf(0L) }
+    val downloadId2 = remember { mutableStateOf(emptyList<Long>()) }
     val isFileDownloaded = remember { mutableStateOf(false) }
+
+
+    val downloadManagerClass = remember { DownloadManagerClass(context) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -85,10 +225,9 @@ fun DownloadSection3(context: Context) {
         LinearProgressIndicator(
             progress = animatedProgress,
             color = Color.Red,
-            trackColor = Color.Yellow
+            trackColor = Color.Yellow,
+            modifier = Modifier.padding(bottom = 18.dp)
         )
-
-        Spacer(modifier = Modifier.padding(top = 18.dp))
 
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
@@ -97,31 +236,32 @@ fun DownloadSection3(context: Context) {
             Button(
                 onClick = {
                     scope.launch {
-                        val fileName = "videos.mp4"
-                        downloadId2.value = downloadManagerClass.startDownload(
-                            "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4",
-                            fileName
+                        downloadId2.value = downloadManagerClass.startDownloads(
+                            singleDownloadUrlList,
+                            fileNames,
+                            false
                         )
-                        showProgressBar.value = true
                     }
                 }
             ) {
                 Text(text = "Download")
             }
+
             Button(
                 onClick = {
-                    scope.launch {
-                        downloadManagerClass.cancelDownload(downloadId2.value)
+                    if (downloadId2.value.isNotEmpty()) {
+
+                        downloadManagerClass.cancelDownloads(downloadId2.value)
                         showProgressBar.value = false
                         downloadProgress.value = 0.0f
-                        downloadId2.value = 0L
+                        downloadId2.value = emptyList()
                         downloadedBytes.value = 0L
                         fileSizeMB.value = 0L
 
                         // Delete the downloaded file from storage
                         val file = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
                         file?.listFiles()?.forEach {
-                            if (it.name == "video.mp4") {
+                            if (it.name == "noob.mp4") {
                                 it.delete()
                             }
                         }
@@ -133,48 +273,85 @@ fun DownloadSection3(context: Context) {
             }
         }
 
-        if (downloadId2.value == 1L) {
+        if (downloadId2.value.isNotEmpty()) {
             Text(text = "Download Completed")
             isFileDownloaded.value = true
         }
         if (showProgressBar.value) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
         }
         if (downloadedBytes.value > 0 && fileSizeMB.value > 0) {
             val downloadedMB = downloadedBytes.value / (1024 * 1024)
             val text = "${downloadedMB}MB/${fileSizeMB.value}MB"
             Text(text = text)
         }
+
     }
 
     LaunchedEffect(downloadId2.value) {
-        while (downloadId2.value != 0L) {
-            when (downloadManagerClass.getDownloadStatus(downloadId2.value)) {
-                DownloadManager.STATUS_SUCCESSFUL -> {
-                    downloadId2.value = 1L
-                    showProgressBar.value = false
-                    downloadedBytes.value = downloadManagerClass.getDownloadedBytes(downloadId2.value)
-                    downloadProgress.value = 1.0f
-                    Utils.showToast(context, "Download Completed")
-                }
-                DownloadManager.STATUS_FAILED -> {
-                    downloadId2.value = 0L
-                    downloadProgress.value = 0.0f
-                    showProgressBar.value = false
-                    Utils.showToast(context, "Download Failed")
-                }
-                else -> {
-                    val progress = downloadManagerClass.getDownloadProgress(downloadId2.value)
-                    if (progress > 0) {
-                        showProgressBar.value = true
-                        downloadProgress.value = progress / 100f
-                        fileSizeMB.value = downloadManagerClass.getFileSizeMB(downloadId2.value)
-                        downloadedBytes.value = downloadManagerClass.getDownloadedBytes(downloadId2.value)
+        while (downloadId2.value.isNotEmpty()) {
+            for (downloadId in downloadId2.value) {
+                when (downloadManagerClass.getCombinedDownloadStatus(listOf(downloadId))) {
+                    DownloadManager.STATUS_SUCCESSFUL -> {
+                        showProgressBar.value = false
+                        downloadedBytes.value = downloadManagerClass.getTotalDownloadedBytes(listOf(downloadId))
+                        downloadProgress.value = 1.0f
+                        Utils.showToast(context, "Download Completed")
+                        downloadId2.value = emptyList()
+                    }
+                    DownloadManager.STATUS_FAILED -> {
+                        downloadProgress.value = 0.0f
+                        showProgressBar.value = false
+                        Utils.showToast(context, "Download Failed")
+                        downloadId2.value = emptyList()
+                    }
+                    else -> {
+                        downloadProgress.value = downloadManagerClass.getDownloadProgress(listOf(downloadId)).toFloat()
+                        if (downloadProgress.value > 0) {
+                            showProgressBar.value = true
+                            downloadProgress.value = downloadProgress.value / 100f
+                            fileSizeMB.value = downloadManagerClass.getTotalFilesSizeMB(listOf(downloadId))
+                            downloadedBytes.value = downloadManagerClass.getTotalDownloadedBytes(listOf(downloadId))
+                        }
                     }
                 }
             }
-            delay(50)
+            delay(50) // Delay to avoid blocking the thread
         }
+    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DownloadScreen() {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Download Section") }
+            )
+        },
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+            ) {
+                DownloadSection3()
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        DownloadScreen()
     }
 }
 
--- app/src/main/java/com/jasmeet/myapplication/downloadManager/DownloadManager.kt
+++ app/src/main/java/com/jasmeet/myapplication/downloadManager/DownloadManager.kt
@@ -2,7 +2,6 @@ package com.jasmeet.myapplication.downloadManager
 
 import android.app.DownloadManager
 import android.content.Context
-import android.net.Uri
 import android.os.Environment
 import androidx.core.net.toUri
 import kotlinx.coroutines.Dispatchers
@@ -11,141 +10,149 @@ import kotlinx.coroutines.withContext
 class DownloadManagerClass(
     private val context: Context
 ) {
-    private val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as android.app.DownloadManager
+    private val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
 
-    suspend fun startDownload(
-        downloadUrl: String,
-        fileName: String,
-    ): Long = withContext(Dispatchers.IO) {
-        val request = DownloadManager.Request(downloadUrl.toUri())
-            .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-            .setAllowedOverRoaming(true)
-            .setTitle("Downloading File")
-            .setDescription("Downloading a file from the internet.")
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            .setDestinationInExternalFilesDir(
-                context,
-                Environment.DIRECTORY_DOWNLOADS,
-                "video.mp4"
-            )
-
+    suspend fun startDownloads(
+        downloadUrls: List<String>,
+        fileNames: List<String>,
+        downloadOnlyOne: Boolean = false
+    ): List<Long> = withContext(Dispatchers.IO) {
+        val downloadIds = mutableListOf<Long>()
         val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        downloadManager.enqueue(request)
-    }
 
-    suspend fun getDownloadStatus(downloadId: Long): Int {
-        val query = DownloadManager.Query().setFilterById(downloadId)
-        val cursor = downloadManager.query(query)
-        val statusIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+        if (downloadOnlyOne && downloadUrls.isNotEmpty()) {
+            val request = DownloadManager.Request(downloadUrls[0].toUri())
+                .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                .setAllowedOverRoaming(true)
+                .setTitle("Downloading File")
+                .setDescription("Downloading a file from the internet.")
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_DOWNLOADS,
+                    "${fileNames[0]}.mp4"
+                )
 
-        if (cursor.moveToFirst()) {
-            if (statusIndex != -1){
-                val status = cursor.getInt(statusIndex)
-                cursor.close()
-                return status
+
+            val downloadId = downloadManager.enqueue(request)
+            downloadIds.add(downloadId)
+        } else {
+            for (i in downloadUrls.indices) {
+                val request = DownloadManager.Request(downloadUrls[i].toUri())
+                    .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                    .setAllowedOverRoaming(true)
+                    .setTitle("Downloading File")
+                    .setDescription("Downloading a file from the internet.")
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                    .setDestinationInExternalPublicDir(
+                        Environment.DIRECTORY_DOWNLOADS,
+                        "${fileNames[i]}.mp4"
+                    )
+
+                val downloadId = downloadManager.enqueue(request)
+                downloadIds.add(downloadId)
             }
         }
-        cursor.close()
-        return -1
+
+        return@withContext downloadIds
     }
 
-    suspend fun getDownloadedFilePath(downloadId: Long): String {
-        val query = DownloadManager.Query().setFilterById(downloadId)
-        val cursor = downloadManager.query(query)
-        val filePathIndex = cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME)
-        if (cursor.moveToFirst()) {
-            if (filePathIndex != -1){
-                val filePath = cursor.getString(filePathIndex)
-                cursor.close()
-                return filePath
+    fun getCombinedDownloadStatus(downloadIds: List<Long>): Int {
+        var combinedStatus = DownloadManager.STATUS_SUCCESSFUL
+
+        for (downloadId in downloadIds) {
+            val query = DownloadManager.Query().setFilterById(downloadId)
+            val cursor = downloadManager.query(query)
+            val statusIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+
+            if (cursor.moveToFirst()) {
+                if (statusIndex != -1) {
+                    val status = cursor.getInt(statusIndex)
+                    if (status != DownloadManager.STATUS_SUCCESSFUL) {
+                        combinedStatus = status
+                    }
+                }
             }
+            cursor.close()
         }
-        cursor.close()
-        return ""
+
+        return combinedStatus
     }
 
-    suspend fun getDownloadProgress(downloadId: Long): Int {
-        val query = DownloadManager.Query().setFilterById(downloadId)
-        val cursor = downloadManager.query(query)
+    fun getDownloadProgress(downloadIds: List<Long>): Int {
+        var totalDownloadedBytes = 0L
+        var totalTotalBytes = 0L
 
-        if (cursor.moveToFirst()) {
+        for (downloadId in downloadIds) {
+            val query = DownloadManager.Query().setFilterById(downloadId)
+            val cursor = downloadManager.query(query)
             val downloadedBytesIndex = cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR)
             val totalBytesIndex = cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES)
 
-            if (downloadedBytesIndex != -1 && totalBytesIndex != -1) {
-                val downloadedBytes = cursor.getInt(downloadedBytesIndex)
-                val totalBytes = cursor.getInt(totalBytesIndex)
-                cursor.close()
-
-                if (totalBytes > 0) {
-                    return (downloadedBytes * 100L / totalBytes).toInt()
+            if (cursor.moveToFirst()) {
+                if (downloadedBytesIndex != -1 && totalBytesIndex != -1) {
+                    totalDownloadedBytes += cursor.getLong(downloadedBytesIndex)
+                    totalTotalBytes += cursor.getLong(totalBytesIndex)
                 }
             }
+            cursor.close()
         }
 
-        cursor.close()
-        return 0
-    }
-
-    //this function will cancel the download
-    suspend fun cancelDownload(downloadId: Long) {
-        downloadManager.remove(downloadId)
-    }
-
-    //this function will handle the error
-    suspend fun handleError(downloadId: Long):String {
-        when (val status =getDownloadStatus(downloadId)) {
-            DownloadManager.STATUS_FAILED -> {
-                return "Download Failed"
-            }
-            DownloadManager.STATUS_PAUSED -> {
-                return "Download Paused"
-            }
-            DownloadManager.STATUS_PENDING -> {
-                return "Download Pending"
-            }
-            DownloadManager.STATUS_RUNNING -> {
-                return "Download Running"
-            }
-            DownloadManager.STATUS_SUCCESSFUL -> {
-                return "Download Successful"
-            }
-            else -> {
-                return "Download Failed"
-            }
+        return if (totalTotalBytes > 0) {
+            ((totalDownloadedBytes * 100L) / totalTotalBytes).toInt()
+        } else {
+            0
         }
     }
 
-    fun getDownloadedBytes(value: Long): Long {
-
-        val query = DownloadManager.Query().setFilterById(value)
-        val cursor = downloadManager.query(query)
-        val downloadedBytesIndex = cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR)
-        if (cursor.moveToFirst()) {
-            if (downloadedBytesIndex != -1){
-                val downloadedBytes = cursor.getLong(downloadedBytesIndex)
-                cursor.close()
-                return downloadedBytes
-            }
+    fun cancelDownloads(downloadIds: List<Long>) {
+        for (downloadId in downloadIds) {
+            downloadManager.remove(downloadId)
         }
-        cursor.close()
-        return 0
     }
 
-    fun getFileSizeMB(value: Long): Long {
-        val query = DownloadManager.Query().setFilterById(value)
-        val cursor = downloadManager.query(query)
-        val totalBytesIndex = cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES)
-        if (cursor.moveToFirst()) {
-            if (totalBytesIndex != -1){
-                val totalBytes = cursor.getLong(totalBytesIndex)
-                cursor.close()
-                return totalBytes / (1024 * 1024)
+
+    fun getTotalDownloadedBytes(downloadIds: List<Long>): Long {
+        var totalDownloadedBytes = 0L
+
+        for (downloadId in downloadIds) {
+            val query = DownloadManager.Query().setFilterById(downloadId)
+            val cursor = downloadManager.query(query)
+            val downloadedBytesIndex = cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR)
+
+            if (cursor.moveToFirst()) {
+                if (downloadedBytesIndex != -1) {
+                    val downloadedBytesValue = cursor.getLong(downloadedBytesIndex)
+                    totalDownloadedBytes += downloadedBytesValue
+                }
             }
+            cursor.close()
         }
-        cursor.close()
-        return 0
+
+        return totalDownloadedBytes
     }
+
+
+    fun getTotalFilesSizeMB(downloadIds: List<Long>): Long {
+        var totalSizeMB = 0L
+
+        for (downloadId in downloadIds) {
+            val query = DownloadManager.Query().setFilterById(downloadId)
+            val cursor = downloadManager.query(query)
+            val totalBytesIndex = cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES)
+
+            if (cursor.moveToFirst()) {
+                if (totalBytesIndex != -1) {
+                    val totalBytes = cursor.getLong(totalBytesIndex)
+                    totalSizeMB += totalBytes / (1024 * 1024)
+                }
+            }
+            cursor.close()
+        }
+
+        return totalSizeMB
+    }
+
+
 
 
 }
